a8aaa99ca95ecfae9b5d6f6ffa74f8e30b28e2a4
jbastidas26 <43036752+jbastidas26@users.noreply.github.com>
2019-03-21 09:06:57 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,36 @@
+
+
+// 1. English Section:
+$(".englishButton").click(function(){
+    var englishInput = $(".english").val();
+        
+    // display this text if the answer is correct
+    $(".englishMessage").text("Correct!");
+    
+    // display this text if the answer is incorrect
+    $(".englishMessage").text("Wrong! Try again."); 
+});
+
+
+// 2. Social Studies section
+$(".ssButton").click(function(){
+    var ssInput = $(".socialStudies").val();
+
+    
+});
+
+
+// 3. Math section
+$(".mathButton").click(function(){
+    var mathInput = $(".math").val();
+    console.log("30");
+    mathInput = parseInt(mathInput);
+
+
+});
+
+
+// 4. Science section
+
+
+
